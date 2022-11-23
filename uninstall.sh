dec7b149b7c13a0e903e418b8ebcd314f88b8da2
theoneVincent <87263818+theoneVincent@users.noreply.github.com>
2022-11-23 10:57:04 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -5,7 +5,6 @@ systemctl disable snell.service
 rm -f /etc/systemd/system/snell.service
 
 cd
-ARCHITECTURE=$(dpkg --print-architecture)
 rm -f snell-server-v4.0.0-linux-amd64.zip
 rm -f snell-server
 rm -f snell-server.conf
